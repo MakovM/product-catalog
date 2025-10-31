fc2c9a5f8ea799a32ba55ad8001f76ea147012e2
MakovM <makovinskiy09@gmail.com>
2025-10-31 15:57:16 +0200

    Add default image for products

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,6 @@
+from django.core.files.storage import default_storage
 from django.db import models
+from django.templatetags.static import static
 
 
 class Category(models.Model):
@@ -19,7 +21,12 @@ class Product(models.Model):
         Category, on_delete=models.CASCADE, related_name="products"
     )
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    image = models.ImageField(
+        upload_to="products/",
+        blank=True,
+        null=True,
+        default="static/img/img.png"
+    )
     description = models.TextField(blank=True)
     stock = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=False)
@@ -28,6 +35,12 @@ class Product(models.Model):
 
     class Meta:
         ordering = ["-available", "name"]
+
+    @property
+    def image_url(self):
+        if self.image and default_storage.exists(self.image.name):
+            return self.image.url
+        return static("img/img.png")
 
     def save(self, *args, **kwargs):
         self.available = self.stock > 0
